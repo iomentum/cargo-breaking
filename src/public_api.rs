c542d68bb9eede8856eff36a9ec7807a7f32985a
Sasha Pourcelot <sasha.pourcelot@iomentum.com>
2021-06-11 09:38:09 +0200

    Add TODOs for future method detection work

--- src/public_api.rs
+++ src/public_api.rs
@@ -160,11 +160,13 @@ fn extract_name_and_generic_args(
 ) -> Option<(&Ident, Option<&AngleBracketedGenericArguments>)> {
     let path = match ty {
         Type::Path(TypePath { path, .. }) => path,
+        // TODO: handle non-path types
         _ => return None,
     };
 
     let unique_segment = match path.segments.len() {
         1 => path.segments.first().unwrap(),
+        // TODO: handle paths with more than one segment in them
         _ => return None,
     };
 
@@ -173,6 +175,7 @@ fn extract_name_and_generic_args(
     let generics = match &unique_segment.arguments {
         syn::PathArguments::None => None,
         syn::PathArguments::AngleBracketed(args) => Some(args),
+        // TODO: handle paths with parenthesis (for instance Fn(T) -> U).
         syn::PathArguments::Parenthesized(_) => return None,
     };
 
