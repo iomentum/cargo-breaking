822716ae2c427befc9b58c06207ddf0fe2279461
Yann <45734971+YannBonaudo@users.noreply.github.com>
2021-07-12 09:09:39 +0200

    Fn arg last comma removed (#13)

--- tests/function.rs
+++ tests/function.rs
@@ -68,6 +68,34 @@ fn body_change_not_detected() {
 }
 
 #[test]
+fn fn_arg_comma_is_removed() {
+    let diff: ApiCompatibilityDiagnostics = parse_quote! {
+        {
+            pub fn a(a: t, b: t, c: t,) {}
+        },
+        {
+            pub fn a(a: t, b: t, c: t) {}
+        },
+    };
+
+    assert!(diff.is_empty());
+}
+
+#[test]
+fn fn_arg_last_character_not_removed() {
+    let diff: ApiCompatibilityDiagnostics = parse_quote! {
+        {
+            pub fn a(a: t, b: t, c: t) {}
+        },
+        {
+            pub fn a(a: t, b: t, c: u) {}
+        },
+    };
+
+    assert_eq!(diff.to_string(), "≠ a\n");
+}
+
+#[test]
 fn empty_struct_kind_change_is_modification() {
     let files = ["pub struct A;", "pub struct A();", "pub struct A {}"];
 
